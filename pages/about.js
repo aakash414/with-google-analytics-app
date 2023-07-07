6a95ba99e24bd560c1aa2eb5818afd751442a983
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-07-07 19:15:52 +0530

    env changed

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
-import Page from '../components/Page'
+import Page from "../components/Page";
 
 export default function About() {
   return (
     <Page>
       <h1>This is the About page</h1>
+      <a href="www.google.com">
+        <button>Google</button>
+      </a>
     </Page>
-  )
+  );
 }
